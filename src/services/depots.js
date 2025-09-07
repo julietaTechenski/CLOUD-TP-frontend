8a1bac363a568b934b94f977ef07f2196aba613c
Julieta Techenski <119064935+julietaTechenski@users.noreply.github.com>
2025-09-07 11:29:47 -0300

    headers added

--- src/services/depots.js
+++ src/services/depots.js
@@ -1,7 +1,14 @@
 const apiUrl = process.env.REACT_APP_API_URL;
 
 export async function getDepots() {
-    const res = await fetch(`${apiUrl}/depots`);
+    const token = "";
+    const res = await fetch(`${apiUrl}/depots`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+        }
+    });
     if (!res.ok) throw new Error("Failed to fetch depots");
     return res.json();
 }
